9e910b60dd2fc048121412bebc358c865adfe94c
olamilekan5162 <opeyemiolalekan481@gmail.com>
2026-07-11 00:23:32 +0100

    add login page scaffold

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Login = () => {
-  return <div>This is the login page</div>;
+  return (
+    <div className="text-2xl font-bold text-white">This is the login page</div>
+  );
 };
 
 export default Login;
